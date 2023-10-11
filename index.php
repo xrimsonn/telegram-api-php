88ef0a3293da7b5be6de9918a366542ce005df88
José Antonio Rosales <jose.rosales.mald@gmail.com>
2023-10-10 19:12:57 -0600

    feat: front end finished

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
 $token = '6448821928:AAEswUb9WaqljpLHjuvw4c0G4TtYZ96VARw'; // Reemplaza con tu token de acceso
 
 $result = false; // Inicializa $result como falso
@@ -35,32 +35,52 @@ if (isset($_POST['chat_id']) && isset($_POST['msg'])) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Telegram API</title>
+  <title>Telegram Bot API</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css" />
   <link rel="stylesheet" href="./font.css">
 </head>
 
 <body>
   <main class="container">
-    <article style="margin-top: 0px">
-    <hgroup>
-      <h1>Sent</h1>
-      <a href="https://www.github.com/xrimsonn">José Antonio Rosales</a>
-    </hgroup>  
+    <nav aria-label="breadcrumb">
+      <ul>
+        <li><a href="https://www.telegram.org/" class="contrast">Telegram</a></li>
+        <li>Bot API</li>
+      </ul>
+    </nav>
+    <article style="margin-top: 0px;">
+      <header>
+        <hgroup>
+          <h1>Sent a message.</h1>
+          <a href="https://www.github.com/xrimsonn">José Antonio Rosales</a>
+        </hgroup>
+      </header>
       <form action="" method="POST">
         <label for="chat_id">Chat ID</label>
-        <input type="text" name="chat_id" id="chat_id" placeholder="Chat ID" required>
+        <input type="text" name="chat_id" id="chat_id" placeholder="6488718466..." required>
         <label for="msg">Mensaje</label>
-        <input type="text" name="msg" id="msg" placeholder="Message" required>
-        <button type="submit">Enviar</button>
+        <input type="text" name="msg" id="msg" placeholder="My message..." required>
+        <button type="submit" class="contrast">Send</button>
       </form>
       <?php if (isset($_POST['chat_id']) && isset($_POST['msg'])) { ?>
         <?php if ($result) { ?>
-          <p>Message sent successfully</p>
+          <p><ins>Message sent successfully</ins></p>
         <?php } else { ?>
-          <p>An error has occurred</p>
+          <p><mark>An error has occurred</mark></p>
         <?php } ?>
       <?php } ?>
+      <footer>
+        <details open>
+          <summary>Steps</summary>
+          <ul>
+            <li>Create a telegram account</li>
+            <li>Get your Chat ID using <kbd>@userinfobot</kbd></li>
+            <li>Sent<code>/start</code>to <kbd>@test_antonnn_bot</kbd></li>
+            <li>Type any message you want and send it</li>
+            <li>Recive the response from the bot</li>
+          </ul>
+        </details>
+      </footer>
     </article>
   </main>
 </body>
